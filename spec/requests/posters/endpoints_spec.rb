845dadfc5461e106246bebc1d95c9711b9c7a384
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-09-04 21:47:01 -0600

    refactor: all poster objects in tests into instance variables.

--- spec/requests/posters/endpoints_spec.rb
+++ spec/requests/posters/endpoints_spec.rb
@@ -9,27 +9,26 @@ RSpec.describe "Poster Endpoints" do
                vintage: true,
                img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d")
 
-    Poster.create!(name: "DOOM",
+    @doom_poster = Poster.create!(name: "DOOM",
                description: "We were poor, my mother and I, living in a world of doom and gloom, pessimism and bitterness, where storms raged and wolves scratched at the door.",
                price: 99.00,
                year: 2001,
                vintage: false,
                img_url:  "https://as1.ftcdn.net/v2/jpg/05/65/34/38/1000_F_565343831_FOY5uGGDcELPRcKpRf4huyrL5DVT4gz8.jpg")
 
-    Poster.create!(name: "EXISTENTIALISM",
+    @existentialism_poster = Poster.create!(name: "EXISTENTIALISM",
                description: "You have your way. I have my way. As for the right way, the correct way, and the only way, it does not exist.",
                price: 69.00,
                year: 2020,
                vintage: true,
                img_url:  "https://wamu.org/wp-content/uploads/2016/03/29/ap_090911089838_wide-58102eebb732a70864dc08c1ff9321af5051b9f4-1500x844.jpg")
 
-    Poster.create!(name: "LONLINESS",
+    @lonely_poster = Poster.create!(name: "LONLINESS",
                description: "To live alone is the fate of all great souls.",
                price: 33.00,
                year: 1991,
                vintage: true,
                img_url:  "https://i.kym-cdn.com/entries/icons/mobile/000/026/213/pablo.jpg")
-
   end
 
   describe 'Verbs' do 
@@ -109,7 +108,19 @@ RSpec.describe "Poster Endpoints" do
     end
 
     it 'can destroy a poster' do
-      
+    
+      # isolate 1 poster
+      # check it's attributes.
+      # run delete verb
+      # check that all attributes are nil
+      #
+      #
+      # delete "/api/v1/posters/#{@lonely_poster.id}"
+
+      expect(response).to be_successful
+
+      poster = JSON.parse(response.body, symbolize_names: true)
+require 'pry'; binding.pry
     end
   end
 end

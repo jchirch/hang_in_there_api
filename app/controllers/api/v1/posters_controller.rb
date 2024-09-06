f0cc37e365cf15624acd2f8f35236320c0d3dcb0
Joel Davalos <jsdavalos@gmail.com>
2024-09-06 17:27:06 -0600

    Logic working in postman for filtering by min and max

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -6,6 +6,10 @@ class Api::V1::PostersController < ApplicationController
         Poster.sort_results(params[:sort])  
       elsif params[:name]
         Poster.filter_name(params[:name])
+      elsif params[:min_price]
+         Poster.filter_min_price(params[:min_price])
+      elsif params[:max_price]
+        Poster.filter_max_price(params[:max_price])
       else
         Poster.all
       end
@@ -41,3 +45,4 @@ class Api::V1::PostersController < ApplicationController
 	end
 end
 
+# Poster.where("price >= #{min_price}")
